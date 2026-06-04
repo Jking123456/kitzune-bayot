4b330860519eef1ebce869029202b8a87c2354eb
Jking123456 <prinzvans@gmail.com>
2026-06-05 00:58:42 +0800

    Update KITSUNE-MLBB.lua

--- KITSUNE-MLBB.lua
+++ KITSUNE-MLBB.lua
@@ -632,11 +632,13 @@ uistart({
     CAtext("• Keep your application up to date continuously.", "#3AA82D", "12sp"),
     CAtext("• Third-party scripts carry inherit user account risks.", "#3AA82D", "12sp"),
     CAtext("• Sharing authenticated key files is strictly restricted.", "#3AA82D", "13sp"),
-    CAline("─────────────────────────")
+    CAline("─────────────────────────\n"),
+    CAbutton('𝚂𝚎𝚕𝚎𝚌𝚝 𝙶𝚊𝚖𝚎 𝙿𝚛𝚘𝚌𝚎𝚜𝚜' function() gg.setProcessX() end),
+    CAtext("Choose Mobile Legends:Bang Bang (UnityKillsMe)", "#666666", "11sp")
   },
 {
   CAtext("🛡 CORE SECURITY & MAP ENGINE", "#00E5FF", "13sp", true),
-  CAswitch("ACTIVATE ANTI REPORT\n", function() antiban() end),
+  CAswitch("ACTIVATE ANTI REPORT", function() antiban() end),
   CAswitch("ACTIVATE ANTI BAN", function() anticheat() end),
   CAtext("Notice: toggle on when you're in lobby.\n", "#666666", "11sp"),
   CAswitch("MAPHACK (NO ICON)", function() map() end),
